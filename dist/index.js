d59f47562c842113cf4c6d7b5d7308c6ff3338de
YassinL <yassin.leclercq1@gmail.com>
2024-05-15 15:25:08 +0200

    v0.0.5

---
+++ dist/index.js
@@ -0,0 +1,128 @@
+"use strict";
+/**
+ * Do not edit directly
+ * Generated on Wed, 15 May 2024 13:05:24 GMT
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.theme = void 0;
+exports.theme = {
+    "color": {
+        "black": "#161617"
+    },
+    "fontFamily": "Montserrat",
+    "lineHeight": {
+        "xs": "1.2rem",
+        "s": "1.4rem",
+        "m": "1.6rem",
+        "l": "1.7rem",
+        "xl": "1.9rem",
+        "xxl": "2.1rem",
+        "xxxl": "2.8rem"
+    },
+    "fontWeight": {
+        "bold": "700",
+        "medium": "500",
+        "semiBold": "600"
+    },
+    "fontSize": {
+        "xxs": "0.75rem",
+        "xs": "0.875rem",
+        "s": "1rem",
+        "m": "1.1875rem",
+        "l": "1.3125rem",
+        "xl": "1.75rem"
+    },
+    "typography": {
+        "title1": {
+            "fontFamily": "Montserrat",
+            "fontWeight": "700",
+            "fontSize": "1.75rem",
+            "color": "#161617"
+        },
+        "title2": {
+            "fontFamily": "Montserrat",
+            "fontWeight": "700",
+            "fontSize": "1.3125rem",
+            "color": "#161617"
+        },
+        "title3": {
+            "fontFamily": "Montserrat",
+            "fontWeight": "700",
+            "fontSize": "1.75rem",
+            "color": "#161617"
+        },
+        "title4": {
+            "fontFamily": "Montserrat",
+            "fontWeight": "700",
+            "fontSize": "1.3125rem",
+            "color": "#161617"
+        },
+        "body": {
+            "fontFamily": "Montserrat",
+            "fontWeight": "500",
+            "fontSize": "1.1875rem",
+            "color": "#161617"
+        },
+        "bodyS": {
+            "fontFamily": "Montserrat",
+            "fontWeight": "500",
+            "fontSize": "1rem",
+            "lineHeight": "1.4rem",
+            "color": "#161617"
+        },
+        "bodyXs": {
+            "fontFamily": "Montserrat",
+            "fontWeight": "500",
+            "fontSize": "0.75rem",
+            "lineHeight": "1.2rem",
+            "color": "#161617"
+        },
+        "bodySemiBold": {
+            "fontFamily": "Montserrat",
+            "fontWeight": "600",
+            "fontSize": "1.1875rem",
+            "color": "#161617"
+        },
+        "bodySemiBoldS": {
+            "fontFamily": "Montserrat",
+            "fontWeight": "600",
+            "fontSize": "0.875rem",
+            "lineHeight": "1.4rem",
+            "color": "#161617"
+        },
+        "bodyItalic": {
+            "fontFamily": "Montserrat",
+            "fontWeight": "500",
+            "fontSize": "1.1875rem",
+            "color": "#161617"
+        },
+        "bodyItalicSemiBold": {
+            "fontFamily": "Montserrat",
+            "fontWeight": "600",
+            "fontSize": "1.1875rem",
+            "color": "#161617"
+        },
+        "bodySemiboldXs": {
+            "fontFamily": "Montserrat",
+            "fontWeight": "600",
+            "fontSize": "0.75rem",
+            "lineHeight": "1.2rem",
+            "color": "#161617"
+        },
+        "link": {
+            "fontFamily": "Montserrat",
+            "fontWeight": "700",
+            "fontSize": "1.1875rem",
+            "textDecoration": "underline",
+            "color": "#161617"
+        },
+        "button": {
+            "fontFamily": "Montserrat",
+            "fontWeight": "700",
+            "fontSize": "1.1875rem",
+            "textTransform": "uppercase",
+            "color": "#161617"
+        }
+    }
+};
+//# sourceMappingURL=index.js.map
