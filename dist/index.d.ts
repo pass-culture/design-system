d0883635d200aed1bb8ff050bfc6a6d5e63787b7
YassinL <yassin.leclercq1@gmail.com>
2024-05-15 15:53:24 +0200

    v0.0.6

---
+++ dist/index.d.ts
@@ -0,0 +1,134 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 15 May 2024 13:49:27 GMT
+ */
+export declare const theme: {
+    readonly color: {
+        readonly black: "#161617";
+    };
+    readonly fontFamily: "Montserrat";
+    readonly lineHeight: {
+        readonly xs: "1.2rem";
+        readonly s: "1.4rem";
+        readonly m: "1.6rem";
+        readonly l: "1.7rem";
+        readonly xl: "1.9rem";
+        readonly xxl: "2.1rem";
+        readonly xxxl: "2.8rem";
+    };
+    readonly fontWeight: {
+        readonly bold: "700";
+        readonly medium: "500";
+        readonly semiBold: "600";
+    };
+    readonly fontSize: {
+        readonly xxs: "0.75rem";
+        readonly xs: "0.875rem";
+        readonly s: "1rem";
+        readonly m: "1.1875rem";
+        readonly l: "1.3125rem";
+        readonly xl: "1.75rem";
+    };
+    readonly typography: {
+        readonly title1: {
+            readonly fontFamily: "Montserrat";
+            readonly fontWeight: "700";
+            readonly fontSize: "1.75rem";
+            readonly lineHeight: "2.8rem";
+            readonly color: "#161617";
+        };
+        readonly title2: {
+            readonly fontFamily: "Montserrat";
+            readonly fontWeight: "700";
+            readonly fontSize: "1.3125rem";
+            readonly lineHeight: "2.1rem";
+            readonly color: "#161617";
+        };
+        readonly title3: {
+            readonly fontFamily: "Montserrat";
+            readonly fontWeight: "700";
+            readonly fontSize: "1.75rem";
+            readonly lineHeight: "1.9rem";
+            readonly color: "#161617";
+        };
+        readonly title4: {
+            readonly fontFamily: "Montserrat";
+            readonly fontWeight: "700";
+            readonly fontSize: "1.3125rem";
+            readonly lineHeight: "1.7rem";
+            readonly color: "#161617";
+        };
+        readonly body: {
+            readonly fontFamily: "Montserrat";
+            readonly fontWeight: "500";
+            readonly fontSize: "1.1875rem";
+            readonly lineHeight: "1.6rem";
+            readonly color: "#161617";
+        };
+        readonly bodyS: {
+            readonly fontFamily: "Montserrat";
+            readonly fontWeight: "500";
+            readonly fontSize: "1rem";
+            readonly lineHeight: "1.4rem";
+            readonly color: "#161617";
+        };
+        readonly bodyXs: {
+            readonly fontFamily: "Montserrat";
+            readonly fontWeight: "500";
+            readonly fontSize: "0.75rem";
+            readonly lineHeight: "1.2rem";
+            readonly color: "#161617";
+        };
+        readonly bodySemiBold: {
+            readonly fontFamily: "Montserrat";
+            readonly fontWeight: "600";
+            readonly fontSize: "1.1875rem";
+            readonly lineHeight: "1.6rem";
+            readonly color: "#161617";
+        };
+        readonly bodySemiBoldS: {
+            readonly fontFamily: "Montserrat";
+            readonly fontWeight: "600";
+            readonly fontSize: "0.875rem";
+            readonly lineHeight: "1.4rem";
+            readonly color: "#161617";
+        };
+        readonly bodyItalic: {
+            readonly fontFamily: "Montserrat";
+            readonly fontWeight: "500";
+            readonly fontSize: "1.1875rem";
+            readonly lineHeight: "1.6rem";
+            readonly color: "#161617";
+        };
+        readonly bodyItalicSemiBold: {
+            readonly fontFamily: "Montserrat";
+            readonly fontWeight: "600";
+            readonly fontSize: "1.1875rem";
+            readonly lineHeight: "1.6rem";
+            readonly color: "#161617";
+        };
+        readonly bodySemiboldXs: {
+            readonly fontFamily: "Montserrat";
+            readonly fontWeight: "600";
+            readonly fontSize: "0.75rem";
+            readonly lineHeight: "1.2rem";
+            readonly color: "#161617";
+        };
+        readonly link: {
+            readonly fontFamily: "Montserrat";
+            readonly fontWeight: "700";
+            readonly fontSize: "1.1875rem";
+            readonly textDecoration: "underline";
+            readonly lineHeight: "1.6rem";
+            readonly color: "#161617";
+        };
+        readonly button: {
+            readonly fontFamily: "Montserrat";
+            readonly fontWeight: "700";
+            readonly fontSize: "1.1875rem";
+            readonly textTransform: "uppercase";
+            readonly lineHeight: "1.6rem";
+            readonly color: "#161617";
+        };
+    };
+};
