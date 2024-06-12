be917873d23c6733773cc3fd079e59eca0c4866d
GuillaumeMgz <guillaume.meigniez@passculture.app>
2024-06-12 15:02:35 +0200

    Include fonts definition in tokens and build a font-face file.

--- build/ts/index.ts
+++ build/ts/index.ts
@@ -1,13 +1,47 @@
 /**
  * Do not edit directly
- * Generated on Wed, 15 May 2024 15:03:35 GMT
+ * Generated on Wed, 12 Jun 2024 12:51:52 GMT
  */
 
 export const theme = {
   "color": {
     "black": "#161617"
   },
-  "fontFamily": "Montserrat",
+  "font": {
+    "bold": {
+      "family": "Montserrat-Bold",
+      "style": "italic",
+      "weight": "700",
+      "src": "url(FONT_URL.woff2) format('woff2')",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
+    },
+    "semiBold": {
+      "family": "Montserrat-SemiBold",
+      "weight": "600",
+      "src": "url(FONT_URL.woff2) format('woff2')",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
+    },
+    "semiBoldItalic": {
+      "family": "Montserrat-SemiBoldItalic",
+      "weight": "600",
+      "style": "italic",
+      "src": "url(FONT_URL.woff2) format('woff2')",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
+    },
+    "medium": {
+      "family": "Montserrat-Medium",
+      "weight": "500",
+      "src": "url(FONT_URL.woff2) format('woff2')",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
+    },
+    "mediumItalic": {
+      "family": "Montserrat-MediumItalic",
+      "style": "italic",
+      "weight": "500",
+      "src": "url(FONT_URL.woff2) format('woff2')",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
+    }
+  },
   "lineHeight": {
     "xs": "1.2rem",
     "s": "1.4rem",
@@ -32,100 +66,86 @@ export const theme = {
   },
   "typography": {
     "title1": {
-      "fontFamily": "Montserrat",
-      "fontWeight": "700",
+      "fontFamily": "Montserrat-Bold",
       "fontSize": "1.75rem",
       "lineHeight": "2.8rem",
       "color": "#161617"
     },
     "title2": {
-      "fontFamily": "Montserrat",
-      "fontWeight": "700",
+      "fontFamily": "Montserrat-Bold",
       "fontSize": "1.3125rem",
       "lineHeight": "2.1rem",
       "color": "#161617"
     },
     "title3": {
-      "fontFamily": "Montserrat",
-      "fontWeight": "700",
+      "fontFamily": "Montserrat-Bold",
       "fontSize": "1.1875rem",
       "lineHeight": "1.9rem",
       "color": "#161617"
     },
     "title4": {
-      "fontFamily": "Montserrat",
-      "fontWeight": "700",
+      "fontFamily": "Montserrat-Bold",
       "fontSize": "1rem",
       "lineHeight": "1.7rem",
       "color": "#161617"
     },
     "body": {
-      "fontFamily": "Montserrat",
-      "fontWeight": "500",
+      "fontFamily": "Montserrat-Medium",
       "fontSize": "1rem",
       "lineHeight": "1.6rem",
       "color": "#161617"
     },
     "bodyS": {
-      "fontFamily": "Montserrat",
-      "fontWeight": "500",
+      "fontFamily": "Montserrat-Medium",
       "fontSize": "0.875rem",
       "lineHeight": "1.4rem",
       "color": "#161617"
     },
     "bodyXs": {
-      "fontFamily": "Montserrat",
-      "fontWeight": "500",
+      "fontFamily": "Montserrat-Medium",
       "fontSize": "0.75rem",
       "lineHeight": "1.2rem",
       "color": "#161617"
     },
     "bodySemiBold": {
-      "fontFamily": "Montserrat",
-      "fontWeight": "600",
+      "fontFamily": "Montserrat-SemiBold",
       "fontSize": "1rem",
       "lineHeight": "1.6rem",
       "color": "#161617"
     },
     "bodySemiBoldS": {
-      "fontFamily": "Montserrat",
-      "fontWeight": "600",
+      "fontFamily": "Montserrat-SemiBold",
       "fontSize": "0.875rem",
       "lineHeight": "1.4rem",
       "color": "#161617"
     },
     "bodyItalic": {
-      "fontFamily": "Montserrat",
-      "fontWeight": "500",
+      "fontFamily": "Montserrat-MediumItalic",
       "fontSize": "1rem",
       "lineHeight": "1.6rem",
       "color": "#161617"
     },
     "bodyItalicSemiBold": {
-      "fontFamily": "Montserrat",
-      "fontWeight": "600",
+      "fontFamily": "Montserrat-SemiBoldItalic",
       "fontSize": "1rem",
       "lineHeight": "1.6rem",
       "color": "#161617"
     },
     "bodySemiboldXs": {
-      "fontFamily": "Montserrat",
-      "fontWeight": "600",
+      "fontFamily": "Montserrat-SemiBold",
       "fontSize": "0.75rem",
       "lineHeight": "1.2rem",
       "color": "#161617"
     },
     "link": {
-      "fontFamily": "Montserrat",
-      "fontWeight": "700",
+      "fontFamily": "Montserrat-Bold",
       "fontSize": "1rem",
       "textDecoration": "underline",
       "lineHeight": "1.6rem",
       "color": "#161617"
     },
     "button": {
-      "fontFamily": "Montserrat",
-      "fontWeight": "700",
+      "fontFamily": "Montserrat-Bold",
       "fontSize": "1rem",
       "textTransform": "uppercase",
       "lineHeight": "1.6rem",
--- src/build.ts
+++ src/build.ts
@@ -1,4 +1,4 @@
-import { formatHelpers, Formatter } from 'style-dictionary'
+import { formatHelpers, Formatter, TransformedToken, TransformedTokens } from 'style-dictionary'
 
 const sd = require('style-dictionary')
 const StyleDictionaryJeune = sd.extend('src/configs/config-jeunes.json')
@@ -9,9 +9,35 @@ const sdJeuneFormatter: Formatter = ({ dictionary, file }) => {
   return `${formatHelpers.fileHeader({ file })}export const theme = ${tokens} as const;\n`
 }
 
+const sdFontFacesFormatter: Formatter = ({ dictionary, file }) => {
+  const fontFacesTokens =
+    (formatHelpers.minifyDictionary(dictionary.tokens) as typeof dictionary.tokens)['font'] ?? {}
+
+  function createFontFace(token: TransformedTokens | TransformedToken) {
+    return [
+      '@font-face {',
+      ` font-family: "${token.family}";`,
+      token.style ? ` font-style: "${token.style}";` : '',
+      token.weight ? ` font-weight: "${token.weight}";` : '',
+      ` src: "${token.src}";`,
+      ` unicode-range: "${token['unicode-range']}";`,
+      '}',
+    ]
+      .filter(Boolean)
+      .join('\n')
+  }
+
+  return Object.values(fontFacesTokens).map(createFontFace).join('\n\n')
+}
+
 StyleDictionaryJeune.registerFormat({
   name: 'typings/es6',
   formatter: sdJeuneFormatter,
+})
+
+StyleDictionaryPro.registerFormat({
+  name: 'css/font-faces',
+  formatter: sdFontFacesFormatter,
 })
 
 StyleDictionaryPro.buildAllPlatforms()
