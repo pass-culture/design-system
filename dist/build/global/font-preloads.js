c9d8564ff25f97b8c27159f03ac922cec23b90de
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-02-10 17:55:11 +0100

    v0.0.17

---
+++ dist/build/global/font-preloads.js
@@ -0,0 +1,9 @@
+"use strict";
+/**
+ * Do not edit directly
+ * Generated on Mon, 10 Feb 2025 16:43:17 GMT
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.fontPreloads = void 0;
+exports.fontPreloads = "<link rel=\"preload\" href=\"https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCuM73w5aXx-p7K4KLg.woff\" as=\"font\" type=\"font/woff2\" crossorigin/>\n<link rel=\"preload\" href=\"https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCu173w5aXx-p7K4KLg.woff\" as=\"font\" type=\"font/woff2\" crossorigin/>\n<link rel=\"preload\" href=\"https://fonts.gstatic.com/s/montserrat/v26/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq3p6WXh0oJC8MLnbtg.woff\" as=\"font\" type=\"font/woff2\" crossorigin/>\n<link rel=\"preload\" href=\"https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtZ6Hw5aXx-p7K4KLg.woff\" as=\"font\" type=\"font/woff2\" crossorigin/>\n<link rel=\"preload\" href=\"https://fonts.gstatic.com/s/montserrat/v26/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq5Z9WXh0oJC8MLnbtg.woff\" as=\"font\" type=\"font/woff2\" crossorigin/>";
+//# sourceMappingURL=font-preloads.js.map
