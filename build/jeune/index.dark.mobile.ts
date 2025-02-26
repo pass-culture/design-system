bfd729fa3c76a5d99253f315bb03da5077bdcaa8
yleclercq-pass <62058919+yleclercq-pass@users.noreply.github.com>
2025-02-26 16:27:16 +0100

    (bsr) fix format
    vscode use prettier format but when we generate file, the build don't use the vscode prettier
    previous build was format with vscode instead of the config

--- build/jeune/index.dark.mobile.ts
+++ build/jeune/index.dark.mobile.ts
@@ -1,205 +1,181 @@
 /**
  * Do not edit directly
- * Generated on Thu, 20 Feb 2025 16:02:39 GMT
+ * Generated on Wed, 26 Feb 2025 15:20:14 GMT
  */
 
 export const theme = {
-  color: {
-    text: {
-      default: '#ffffff',
-      subtle: '#f1f1f4',
-      error: '#f8b2c0',
-      disabled: '#cbcdd2',
-      success: '#91e1b5',
-      'brand-primary': '#ffe0ea',
-      'brand-primary-hover': '#ffa5c0',
-      'brand-secondary': '#320096',
-      'brand-secondary-hover': '#25026c',
-      inverted: '#161617',
-      'locked-default': '#ffffff',
+  "color": {
+    "text": {
+      "default": "#ffffff",
+      "subtle": "#f1f1f4",
+      "error": "#f8b2c0",
+      "disabled": "#cbcdd2",
+      "success": "#91e1b5",
+      "brand-primary": "#ffe0ea",
+      "brand-primary-hover": "#ffa5c0",
+      "brand-secondary": "#320096",
+      "brand-secondary-hover": "#25026c",
+      "inverted": "#161617",
+      "locked-default": "#ffffff"
     },
-    border: {
-      default: '#cbcdd2',
-      subtle: '#90949d',
-      disabled: '#90949d',
-      error: '#f8b2c0',
-      focused: '#ffffff',
-      'brand-primary': '#f2497c',
-      'brand-primary-selected': '#f2497c',
-      'brand-primary-hover': '#eb0055',
+    "border": {
+      "default": "#cbcdd2",
+      "subtle": "#90949d",
+      "disabled": "#90949d",
+      "error": "#f8b2c0",
+      "focused": "#ffffff",
+      "brand-primary": "#f2497c",
+      "brand-primary-selected": "#f2497c",
+      "brand-primary-hover": "#eb0055"
     },
-    outline: {
-      default: '#ffffff',
-      error: '#f83552',
-      inverted: '#161617',
+    "outline": {
+      "default": "#ffffff",
+      "error": "#f83552",
+      "inverted": "#161617"
     },
-    background: {
-      default: '#161617',
-      subtle: '#696a6f',
-      disabled: '#696a6f',
-      error: '#a20121',
-      success: '#15884f',
-      warning: '#e1a605',
-      info: '#eb0055',
-      'brand-primary': '#ffa5c0',
-      'brand-primary-hover': '#f2497c',
-      'brand-primary-selected': '#eb0055',
-      'locked-default': '#161617',
-      'locked-brand-primary': '#eb0055',
-      'locked-brand-secondary': '#320096',
+    "background": {
+      "default": "#161617",
+      "subtle": "#696a6f",
+      "disabled": "#696a6f",
+      "error": "#a20121",
+      "success": "#15884f",
+      "warning": "#e1a605",
+      "info": "#eb0055",
+      "brand-primary": "#ffa5c0",
+      "brand-primary-hover": "#f2497c",
+      "brand-primary-selected": "#eb0055",
+      "locked-default": "#161617",
+      "locked-brand-primary": "#eb0055",
+      "locked-brand-secondary": "#320096"
     },
-    icon: {
-      default: '#ffffff',
-      disabled: '#cbcdd2',
-      subtle: '#f1f1f4',
-      info: '#ffe0ea',
-      error: '#f8b2c0',
-      success: '#eaf8f0',
-      warning: '#fff8df',
-      'brand-primary': '#ffe0ea',
-      'brand-primary-hover': '#ffa5c0',
-      inverted: '#161617',
-    },
+    "icon": {
+      "default": "#ffffff",
+      "disabled": "#cbcdd2",
+      "subtle": "#f1f1f4",
+      "info": "#ffe0ea",
+      "error": "#f8b2c0",
+      "success": "#eaf8f0",
+      "warning": "#fff8df",
+      "brand-primary": "#ffe0ea",
+      "brand-primary-hover": "#ffa5c0",
+      "inverted": "#161617"
+    }
   },
-  separator: {
-    color: {
-      default: '#cbcdd2',
-    },
+  "separator": {
+    "color": {
+      "default": "#cbcdd2"
+    }
   },
-  font: {
-    bold: {
-      family: 'Montserrat-Bold',
-      weight: '700',
-      display: 'swap',
-      style: 'normal',
-      src: 'https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCuM73w5aXx-p7K4KLg.woff',
-      'unicode-range':
-        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;',
+  "font": {
+    "bold": {
+      "family": "Montserrat-Bold",
+      "weight": "700",
+      "display": "swap",
+      "style": "normal",
+      "src": "https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCuM73w5aXx-p7K4KLg.woff",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
     },
-    semiBold: {
-      family: 'Montserrat-SemiBold',
-      weight: '600',
-      display: 'swap',
-      style: 'normal',
-      src: 'https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCu173w5aXx-p7K4KLg.woff',
-      'unicode-range':
-        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;',
+    "semiBold": {
+      "family": "Montserrat-SemiBold",
+      "weight": "600",
+      "display": "swap",
+      "style": "normal",
+      "src": "https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCu173w5aXx-p7K4KLg.woff",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
     },
-    semiBoldItalic: {
-      family: 'Montserrat-SemiBoldItalic',
-      weight: '600',
-      display: 'swap',
-      style: 'italic',
-      src: 'https://fonts.gstatic.com/s/montserrat/v26/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq3p6WXh0oJC8MLnbtg.woff',
-      'unicode-range':
-        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;',
+    "semiBoldItalic": {
+      "family": "Montserrat-SemiBoldItalic",
+      "weight": "600",
+      "display": "swap",
+      "style": "italic",
+      "src": "https://fonts.gstatic.com/s/montserrat/v26/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq3p6WXh0oJC8MLnbtg.woff",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
     },
-    medium: {
-      family: 'Montserrat-Medium',
-      weight: '500',
-      display: 'swap',
-      style: 'normal',
-      src: 'https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtZ6Hw5aXx-p7K4KLg.woff',
-      'unicode-range':
-        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;',
+    "medium": {
+      "family": "Montserrat-Medium",
+      "weight": "500",
+      "display": "swap",
+      "style": "normal",
+      "src": "https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtZ6Hw5aXx-p7K4KLg.woff",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
     },
-    mediumItalic: {
-      family: 'Montserrat-MediumItalic',
-      weight: '500',
-      style: 'italic',
-      display: 'swap',
-      src: 'https://fonts.gstatic.com/s/montserrat/v26/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq5Z9WXh0oJC8MLnbtg.woff',
-      'unicode-range':
-        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;',
-    },
+    "mediumItalic": {
+      "family": "Montserrat-MediumItalic",
+      "weight": "500",
+      "style": "italic",
+      "display": "swap",
+      "src": "https://fonts.gstatic.com/s/montserrat/v26/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq5Z9WXh0oJC8MLnbtg.woff",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
+    }
   },
-  typography: {
-    title1: {
-      fontFamily: 'Montserrat-Bold',
-      fontSize: '28px',
-      lineHeight: '44.8px',
+  "typography": {
+    "title1": {
+      "fontFamily": "Montserrat-Bold",
+      "fontSize": "28px",
+      "lineHeight": "44.8px"
     },
-    title2: {
-      fontFamily: 'Montserrat-Bold',
-      fontSize: '21px',
-      lineHeight: '33.6px',
+    "title2": {
+      "fontFamily": "Montserrat-Bold",
+      "fontSize": "21px",
+      "lineHeight": "33.6px"
     },
-    title3: {
-      fontFamily: 'Montserrat-Bold',
-      fontSize: '19px',
-      lineHeight: '30.4px',
+    "title3": {
+      "fontFamily": "Montserrat-Bold",
+      "fontSize": "19px",
+      "lineHeight": "30.4px"
     },
-    title4: {
-      fontFamily: 'Montserrat-Bold',
-      fontSize: '17px',
-      lineHeight: '27.2px',
+    "title4": {
+      "fontFamily": "Montserrat-Bold",
+      "fontSize": "17px",
+      "lineHeight": "27.2px"
     },
-    body: {
-      fontFamily: 'Montserrat-Medium',
-      fontSize: '16px',
-      lineHeight: '25.6px',
+    "body": {
+      "fontFamily": "Montserrat-Medium",
+      "fontSize": "16px",
+      "lineHeight": "25.6px"
     },
-    bodyS: {
-      fontFamily: 'Montserrat-Medium',
-      fontSize: '14px',
-      lineHeight: '22.4px',
+    "bodyS": {
+      "fontFamily": "Montserrat-Medium",
+      "fontSize": "14px",
+      "lineHeight": "22.4px"
     },
-    bodyXs: {
-      fontFamily: 'Montserrat-Medium',
-      fontSize: '12px',
-      lineHeight: '19.2px',
+    "bodyXs": {
+      "fontFamily": "Montserrat-Medium",
+      "fontSize": "12px",
+      "lineHeight": "19.2px"
     },
-    bodyAccent: {
-      fontFamily: 'Montserrat-SemiBold',
-      fontSize: '16px',
-      lineHeight: '25.6px',
+    "bodyAccent": {
+      "fontFamily": "Montserrat-SemiBold",
+      "fontSize": "16px",
+      "lineHeight": "25.6px"
     },
-    bodyAccentS: {
-      fontFamily: 'Montserrat-SemiBold',
-      fontSize: '14px',
-      lineHeight: '22.4px',
+    "bodyAccentS": {
+      "fontFamily": "Montserrat-SemiBold",
+      "fontSize": "14px",
+      "lineHeight": "22.4px"
     },
-    bodyAccentXs: {
-      fontFamily: 'Montserrat-SemiBold',
-      fontSize: '12px',
-      lineHeight: '19.2px',
+    "bodyAccentXs": {
+      "fontFamily": "Montserrat-SemiBold",
+      "fontSize": "12px",
+      "lineHeight": "19.2px"
     },
-    bodyItalic: {
-      fontFamily: 'Montserrat-MediumItalic',
-      fontSize: '16px',
-      lineHeight: '25.6px',
+    "bodyItalic": {
+      "fontFamily": "Montserrat-MediumItalic",
+      "fontSize": "16px",
+      "lineHeight": "25.6px"
     },
-    bodyItalicAccent: {
-      fontFamily: 'Montserrat-SemiBoldItalic',
-      fontSize: '16px',
-      lineHeight: '25.6px',
+    "bodyItalicAccent": {
+      "fontFamily": "Montserrat-SemiBoldItalic",
+      "fontSize": "16px",
+      "lineHeight": "25.6px"
     },
-    button: {
-      fontFamily: 'Montserrat-Bold',
-      fontSize: '16px',
-      lineHeight: '25.6px',
-    },
-  },
-} as const
+    "button": {
+      "fontFamily": "Montserrat-Bold",
+      "fontSize": "16px",
+      "lineHeight": "25.6px"
+    }
+  }
+} as const;
 
-export type ColorsType =
-  | '#ffffff'
-  | '#f1f1f4'
-  | '#f8b2c0'
-  | '#cbcdd2'
-  | '#91e1b5'
-  | '#ffe0ea'
-  | '#ffa5c0'
-  | '#320096'
-  | '#25026c'
-  | '#161617'
-  | '#90949d'
-  | '#f2497c'
-  | '#eb0055'
-  | '#f83552'
-  | '#696a6f'
-  | '#a20121'
-  | '#15884f'
-  | '#e1a605'
-  | '#eaf8f0'
-  | '#fff8df'
+export type ColorsType = "#ffffff" | "#f1f1f4" | "#f8b2c0" | "#cbcdd2" | "#91e1b5" | "#ffe0ea" | "#ffa5c0" | "#320096" | "#25026c" | "#161617" | "#90949d" | "#f2497c" | "#eb0055" | "#f83552" | "#696a6f" | "#a20121" | "#15884f" | "#e1a605" | "#eaf8f0" | "#fff8df";
+      
--- build/pro/index.dark.web.ts
+++ build/pro/index.dark.web.ts
@@ -1,204 +1,181 @@
 /**
  * Do not edit directly
- * Generated on Thu, 20 Feb 2025 16:02:39 GMT
+ * Generated on Wed, 26 Feb 2025 15:20:14 GMT
  */
 
 export const theme = {
-  color: {
-    text: {
-      default: '#ffffff',
-      subtle: '#f1f1f4',
-      error: '#f8b2c0',
-      disabled: '#cbcdd2',
-      success: '#91e1b5',
-      'brand-primary': '#f3edff',
-      'brand-primary-hover': '#c1a3ff',
-      'brand-secondary': '#320096',
-      'brand-secondary-hover': '#25026c',
-      inverted: '#161617',
-      'locked-default': '#ffffff',
+  "color": {
+    "text": {
+      "default": "#ffffff",
+      "subtle": "#f1f1f4",
+      "error": "#f8b2c0",
+      "disabled": "#cbcdd2",
+      "success": "#91e1b5",
+      "brand-primary": "#f3edff",
+      "brand-primary-hover": "#c1a3ff",
+      "brand-secondary": "#320096",
+      "brand-secondary-hover": "#25026c",
+      "inverted": "#161617",
+      "locked-default": "#ffffff"
     },
-    border: {
-      default: '#cbcdd2',
-      subtle: '#90949d',
-      disabled: '#90949d',
-      error: '#f8b2c0',
-      focused: '#ffffff',
-      'brand-primary': '#6123df',
-      'brand-primary-selected': '#6123df',
-      'brand-primary-hover': '#320096',
+    "border": {
+      "default": "#cbcdd2",
+      "subtle": "#90949d",
+      "disabled": "#90949d",
+      "error": "#f8b2c0",
+      "focused": "#ffffff",
+      "brand-primary": "#6123df",
+      "brand-primary-selected": "#6123df",
+      "brand-primary-hover": "#320096"
     },
-    outline: {
-      default: '#ffffff',
-      error: '#f83552',
-      inverted: '#161617',
+    "outline": {
+      "default": "#ffffff",
+      "error": "#f83552",
+      "inverted": "#161617"
     },
-    background: {
-      default: '#161617',
-      subtle: '#696a6f',
-      disabled: '#696a6f',
-      error: '#a20121',
-      success: '#15884f',
-      warning: '#e1a605',
-      info: '#320096',
-      'brand-primary': '#c1a3ff',
-      'brand-primary-hover': '#6123df',
-      'brand-primary-selected': '#320096',
-      'locked-default': '#161617',
-      'locked-brand-primary': '#320096',
-      'locked-brand-secondary': '#320096',
+    "background": {
+      "default": "#161617",
+      "subtle": "#696a6f",
+      "disabled": "#696a6f",
+      "error": "#a20121",
+      "success": "#15884f",
+      "warning": "#e1a605",
+      "info": "#320096",
+      "brand-primary": "#c1a3ff",
+      "brand-primary-hover": "#6123df",
+      "brand-primary-selected": "#320096",
+      "locked-default": "#161617",
+      "locked-brand-primary": "#320096",
+      "locked-brand-secondary": "#320096"
     },
-    icon: {
-      default: '#ffffff',
-      disabled: '#cbcdd2',
-      subtle: '#f1f1f4',
-      info: '#f3edff',
-      error: '#f8b2c0',
-      success: '#eaf8f0',
-      warning: '#fff8df',
-      'brand-primary': '#f3edff',
-      'brand-primary-hover': '#c1a3ff',
-      inverted: '#161617',
-    },
+    "icon": {
+      "default": "#ffffff",
+      "disabled": "#cbcdd2",
+      "subtle": "#f1f1f4",
+      "info": "#f3edff",
+      "error": "#f8b2c0",
+      "success": "#eaf8f0",
+      "warning": "#fff8df",
+      "brand-primary": "#f3edff",
+      "brand-primary-hover": "#c1a3ff",
+      "inverted": "#161617"
+    }
   },
-  separator: {
-    color: {
-      default: '#cbcdd2',
-    },
+  "separator": {
+    "color": {
+      "default": "#cbcdd2"
+    }
   },
-  font: {
-    bold: {
-      family: 'Montserrat-Bold',
-      weight: '700',
-      display: 'swap',
-      style: 'normal',
-      src: 'https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCuM73w5aXx-p7K4KLg.woff',
-      'unicode-range':
-        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;',
+  "font": {
+    "bold": {
+      "family": "Montserrat-Bold",
+      "weight": "700",
+      "display": "swap",
+      "style": "normal",
+      "src": "https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCuM73w5aXx-p7K4KLg.woff",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
     },
-    semiBold: {
-      family: 'Montserrat-SemiBold',
-      weight: '600',
-      display: 'swap',
-      style: 'normal',
-      src: 'https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCu173w5aXx-p7K4KLg.woff',
-      'unicode-range':
-        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;',
+    "semiBold": {
+      "family": "Montserrat-SemiBold",
+      "weight": "600",
+      "display": "swap",
+      "style": "normal",
+      "src": "https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCu173w5aXx-p7K4KLg.woff",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
     },
-    semiBoldItalic: {
-      family: 'Montserrat-SemiBoldItalic',
-      weight: '600',
-      display: 'swap',
-      style: 'italic',
-      src: 'https://fonts.gstatic.com/s/montserrat/v26/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq3p6WXh0oJC8MLnbtg.woff',
-      'unicode-range':
-        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;',
+    "semiBoldItalic": {
+      "family": "Montserrat-SemiBoldItalic",
+      "weight": "600",
+      "display": "swap",
+      "style": "italic",
+      "src": "https://fonts.gstatic.com/s/montserrat/v26/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq3p6WXh0oJC8MLnbtg.woff",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
     },
-    medium: {
-      family: 'Montserrat-Medium',
-      weight: '500',
-      display: 'swap',
-      style: 'normal',
-      src: 'https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtZ6Hw5aXx-p7K4KLg.woff',
-      'unicode-range':
-        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;',
+    "medium": {
+      "family": "Montserrat-Medium",
+      "weight": "500",
+      "display": "swap",
+      "style": "normal",
+      "src": "https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtZ6Hw5aXx-p7K4KLg.woff",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
     },
-    mediumItalic: {
-      family: 'Montserrat-MediumItalic',
-      weight: '500',
-      style: 'italic',
-      display: 'swap',
-      src: 'https://fonts.gstatic.com/s/montserrat/v26/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq5Z9WXh0oJC8MLnbtg.woff',
-      'unicode-range':
-        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;',
-    },
+    "mediumItalic": {
+      "family": "Montserrat-MediumItalic",
+      "weight": "500",
+      "style": "italic",
+      "display": "swap",
+      "src": "https://fonts.gstatic.com/s/montserrat/v26/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq5Z9WXh0oJC8MLnbtg.woff",
+      "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;"
+    }
   },
-  typography: {
-    title1: {
-      fontFamily: 'Montserrat-Bold',
-      fontSize: '1.75rem',
-      lineHeight: '2.8rem',
+  "typography": {
+    "title1": {
+      "fontFamily": "Montserrat-Bold",
+      "fontSize": "1.75rem",
+      "lineHeight": "2.8rem"
     },
-    title2: {
-      fontFamily: 'Montserrat-Bold',
-      fontSize: '1.3125rem',
-      lineHeight: '2.1rem',
+    "title2": {
+      "fontFamily": "Montserrat-Bold",
+      "fontSize": "1.3125rem",
+      "lineHeight": "2.1rem"
     },
-    title3: {
-      fontFamily: 'Montserrat-Bold',
-      fontSize: '1.1875rem',
-      lineHeight: '1.9rem',
+    "title3": {
+      "fontFamily": "Montserrat-Bold",
+      "fontSize": "1.1875rem",
+      "lineHeight": "1.9rem"
     },
-    title4: {
-      fontFamily: 'Montserrat-Bold',
-      fontSize: '1.0625rem',
-      lineHeight: '1.7rem',
+    "title4": {
+      "fontFamily": "Montserrat-Bold",
+      "fontSize": "1.0625rem",
+      "lineHeight": "1.7rem"
     },
-    body: {
-      fontFamily: 'Montserrat-Medium',
-      fontSize: '1rem',
-      lineHeight: '1.6rem',
+    "body": {
+      "fontFamily": "Montserrat-Medium",
+      "fontSize": "1rem",
+      "lineHeight": "1.6rem"
     },
-    bodyS: {
-      fontFamily: 'Montserrat-Medium',
-      fontSize: '0.875rem',
-      lineHeight: '1.4rem',
+    "bodyS": {
+      "fontFamily": "Montserrat-Medium",
+      "fontSize": "0.875rem",
+      "lineHeight": "1.4rem"
     },
-    bodyXs: {
-      fontFamily: 'Montserrat-Medium',
-      fontSize: '0.75rem',
-      lineHeight: '1.2rem',
+    "bodyXs": {
+      "fontFamily": "Montserrat-Medium",
+      "fontSize": "0.75rem",
+      "lineHeight": "1.2rem"
     },
-    bodyAccent: {
-      fontFamily: 'Montserrat-SemiBold',
-      fontSize: '1rem',
-      lineHeight: '1.6rem',
+    "bodyAccent": {
+      "fontFamily": "Montserrat-SemiBold",
+      "fontSize": "1rem",
+      "lineHeight": "1.6rem"
     },
-    bodyAccentS: {
-      fontFamily: 'Montserrat-SemiBold',
-      fontSize: '0.875rem',
-      lineHeight: '1.4rem',
+    "bodyAccentS": {
+      "fontFamily": "Montserrat-SemiBold",
+      "fontSize": "0.875rem",
+      "lineHeight": "1.4rem"
     },
-    bodyAccentXs: {
-      fontFamily: 'Montserrat-SemiBold',
-      fontSize: '0.75rem',
-      lineHeight: '1.2rem',
+    "bodyAccentXs": {
+      "fontFamily": "Montserrat-SemiBold",
+      "fontSize": "0.75rem",
+      "lineHeight": "1.2rem"
     },
-    bodyItalic: {
-      fontFamily: 'Montserrat-MediumItalic',
-      fontSize: '1rem',
-      lineHeight: '1.6rem',
+    "bodyItalic": {
+      "fontFamily": "Montserrat-MediumItalic",
+      "fontSize": "1rem",
+      "lineHeight": "1.6rem"
     },
-    bodyItalicAccent: {
-      fontFamily: 'Montserrat-SemiBoldItalic',
-      fontSize: '1rem',
-      lineHeight: '1.6rem',
+    "bodyItalicAccent": {
+      "fontFamily": "Montserrat-SemiBoldItalic",
+      "fontSize": "1rem",
+      "lineHeight": "1.6rem"
     },
-    button: {
-      fontFamily: 'Montserrat-Bold',
-      fontSize: '1rem',
-      lineHeight: '1.6rem',
-    },
-  },
-} as const
+    "button": {
+      "fontFamily": "Montserrat-Bold",
+      "fontSize": "1rem",
+      "lineHeight": "1.6rem"
+    }
+  }
+} as const;
 
-export type ColorsType =
-  | '#ffffff'
-  | '#f1f1f4'
-  | '#f8b2c0'
-  | '#cbcdd2'
-  | '#91e1b5'
-  | '#f3edff'
-  | '#c1a3ff'
-  | '#320096'
-  | '#25026c'
-  | '#161617'
-  | '#90949d'
-  | '#6123df'
-  | '#f83552'
-  | '#696a6f'
-  | '#a20121'
-  | '#15884f'
-  | '#e1a605'
-  | '#eaf8f0'
-  | '#fff8df'
+export type ColorsType = "#ffffff" | "#f1f1f4" | "#f8b2c0" | "#cbcdd2" | "#91e1b5" | "#f3edff" | "#c1a3ff" | "#320096" | "#25026c" | "#161617" | "#90949d" | "#6123df" | "#f83552" | "#696a6f" | "#a20121" | "#15884f" | "#e1a605" | "#eaf8f0" | "#fff8df";
+      
