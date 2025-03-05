acade02eaf5692e94966cc02087a742a17f5cb31
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-03-05 18:09:11 +0100

    v0.0.21

---
+++ dist/build/jeune/index.dark.web.d.ts
@@ -0,0 +1,180 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 05 Mar 2025 17:04:05 GMT
+ */
+export declare const theme: {
+    readonly color: {
+        readonly text: {
+            readonly default: "#ffffff";
+            readonly subtle: "#90949d";
+            readonly error: "#f83552";
+            readonly disabled: "#f1f1f4";
+            readonly success: "#2cbe6e";
+            readonly "brand-primary": "#ffa5c0";
+            readonly "brand-primary-hover": "#f2497c";
+            readonly "brand-secondary": "#c1a3ff";
+            readonly "brand-secondary-hover": "#f3edff";
+            readonly inverted: "#161617";
+            readonly "locked-default": "#ffffff";
+        };
+        readonly border: {
+            readonly default: "#cbcdd2";
+            readonly subtle: "#90949d";
+            readonly disabled: "#90949d";
+            readonly error: "#f83552";
+            readonly focused: "#ffffff";
+            readonly "brand-primary": "#ffa5c0";
+            readonly "brand-primary-selected": "#ffa5c0";
+            readonly "brand-primary-hover": "#f2497c";
+            readonly selected: "#ffffff";
+        };
+        readonly outline: {
+            readonly default: "#ffffff";
+            readonly error: "#f83552";
+            readonly inverted: "#161617";
+        };
+        readonly background: {
+            readonly default: "#161617";
+            readonly subtle: "#696a6f";
+            readonly disabled: "#696a6f";
+            readonly error: "#700118";
+            readonly success: "#09532d";
+            readonly warning: "#664900";
+            readonly info: "#cb0659";
+            readonly "brand-primary": "#ffa5c0";
+            readonly "brand-primary-hover": "#ffe0ea";
+            readonly "brand-primary-selected": "#cb0659";
+            readonly "locked-default": "#ffffff";
+            readonly "locked-brand-primary": "#eb0055";
+            readonly "locked-brand-secondary": "#ffa5c0";
+        };
+        readonly icon: {
+            readonly default: "#ffffff";
+            readonly disabled: "#cbcdd2";
+            readonly subtle: "#cbcdd2";
+            readonly info: "#ffa5c0";
+            readonly error: "#f83552";
+            readonly success: "#2cbe6e";
+            readonly warning: "#ffef63";
+            readonly "brand-primary": "#ffa5c0";
+            readonly "brand-primary-hover": "#ffe0ea";
+            readonly inverted: "#161617";
+        };
+    };
+    readonly separator: {
+        readonly color: {
+            readonly default: "#90949d";
+            readonly subtle: "#696a6f";
+        };
+    };
+    readonly font: {
+        readonly bold: {
+            readonly family: "Montserrat-Bold";
+            readonly weight: "700";
+            readonly display: "swap";
+            readonly style: "normal";
+            readonly src: "https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCuM73w5aXx-p7K4KLg.woff";
+            readonly "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;";
+        };
+        readonly semiBold: {
+            readonly family: "Montserrat-SemiBold";
+            readonly weight: "600";
+            readonly display: "swap";
+            readonly style: "normal";
+            readonly src: "https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCu173w5aXx-p7K4KLg.woff";
+            readonly "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;";
+        };
+        readonly semiBoldItalic: {
+            readonly family: "Montserrat-SemiBoldItalic";
+            readonly weight: "600";
+            readonly display: "swap";
+            readonly style: "italic";
+            readonly src: "https://fonts.gstatic.com/s/montserrat/v26/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq3p6WXh0oJC8MLnbtg.woff";
+            readonly "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;";
+        };
+        readonly medium: {
+            readonly family: "Montserrat-Medium";
+            readonly weight: "500";
+            readonly display: "swap";
+            readonly style: "normal";
+            readonly src: "https://fonts.gstatic.com/s/montserrat/v26/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtZ6Hw5aXx-p7K4KLg.woff";
+            readonly "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;";
+        };
+        readonly mediumItalic: {
+            readonly family: "Montserrat-MediumItalic";
+            readonly weight: "500";
+            readonly style: "italic";
+            readonly display: "swap";
+            readonly src: "https://fonts.gstatic.com/s/montserrat/v26/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq5Z9WXh0oJC8MLnbtg.woff";
+            readonly "unicode-range": "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;";
+        };
+    };
+    readonly typography: {
+        readonly title1: {
+            readonly fontFamily: "Montserrat-Bold";
+            readonly fontSize: "1.75rem";
+            readonly lineHeight: "2.8rem";
+        };
+        readonly title2: {
+            readonly fontFamily: "Montserrat-Bold";
+            readonly fontSize: "1.3125rem";
+            readonly lineHeight: "2.1rem";
+        };
+        readonly title3: {
+            readonly fontFamily: "Montserrat-Bold";
+            readonly fontSize: "1.1875rem";
+            readonly lineHeight: "1.9rem";
+        };
+        readonly title4: {
+            readonly fontFamily: "Montserrat-Bold";
+            readonly fontSize: "1.0625rem";
+            readonly lineHeight: "1.7rem";
+        };
+        readonly body: {
+            readonly fontFamily: "Montserrat-Medium";
+            readonly fontSize: "1rem";
+            readonly lineHeight: "1.6rem";
+        };
+        readonly bodyS: {
+            readonly fontFamily: "Montserrat-Medium";
+            readonly fontSize: "0.875rem";
+            readonly lineHeight: "1.4rem";
+        };
+        readonly bodyXs: {
+            readonly fontFamily: "Montserrat-Medium";
+            readonly fontSize: "0.75rem";
+            readonly lineHeight: "1.2rem";
+        };
+        readonly bodyAccent: {
+            readonly fontFamily: "Montserrat-SemiBold";
+            readonly fontSize: "1rem";
+            readonly lineHeight: "1.6rem";
+        };
+        readonly bodyAccentS: {
+            readonly fontFamily: "Montserrat-SemiBold";
+            readonly fontSize: "0.875rem";
+            readonly lineHeight: "1.4rem";
+        };
+        readonly bodyAccentXs: {
+            readonly fontFamily: "Montserrat-SemiBold";
+            readonly fontSize: "0.75rem";
+            readonly lineHeight: "1.2rem";
+        };
+        readonly bodyItalic: {
+            readonly fontFamily: "Montserrat-MediumItalic";
+            readonly fontSize: "1rem";
+            readonly lineHeight: "1.6rem";
+        };
+        readonly bodyItalicAccent: {
+            readonly fontFamily: "Montserrat-SemiBoldItalic";
+            readonly fontSize: "1rem";
+            readonly lineHeight: "1.6rem";
+        };
+        readonly button: {
+            readonly fontFamily: "Montserrat-Bold";
+            readonly fontSize: "1rem";
+            readonly lineHeight: "1.6rem";
+        };
+    };
+};
+export type ColorsType = "#ffffff" | "#90949d" | "#f83552" | "#f1f1f4" | "#2cbe6e" | "#ffa5c0" | "#f2497c" | "#c1a3ff" | "#f3edff" | "#161617" | "#cbcdd2" | "#696a6f" | "#700118" | "#09532d" | "#664900" | "#cb0659" | "#ffe0ea" | "#eb0055" | "#ffef63";
