0d4bdcd6a537b019335b7d0d1bf7fef821fe1f94
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-04-03 15:32:39 +0200

    v0.0.25

---
+++ dist/build/jeune/index.dark.web.d.ts
@@ -0,0 +1,138 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ * Generated on Thu, 03 Apr 2025 13:31:09 GMT
+ */
+export declare const theme: {
+    readonly color: {
+        readonly text: {
+            readonly default: "#ffffff";
+            readonly subtle: "#cbcdd2";
+            readonly error: "#f83552";
+            readonly disabled: "#f1f1f4";
+            readonly success: "#2cbe6e";
+            readonly brandPrimary: "#ffa5c0";
+            readonly brandPrimaryHover: "#f2497c";
+            readonly brandSecondary: "#c1a3ff";
+            readonly brandSecondaryHover: "#6123df";
+            readonly inverted: "#161617";
+            readonly locked: "#ffffff";
+        };
+        readonly border: {
+            readonly default: "#cbcdd2";
+            readonly subtle: "#90949d";
+            readonly disabled: "#696a6f";
+            readonly error: "#f83552";
+            readonly selected: "#ffffff";
+            readonly focused: "#ffffff";
+            readonly brandPrimary: "#ffa5c0";
+            readonly brandPrimaryHover: "#f2497c";
+        };
+        readonly outline: {
+            readonly default: "#ffffff";
+            readonly error: "#f83552";
+            readonly inverted: "#161617";
+        };
+        readonly background: {
+            readonly default: "#161617";
+            readonly subtle: "#696a6f";
+            readonly disabled: "#696a6f";
+            readonly error: "#700118";
+            readonly success: "#09532d";
+            readonly warning: "#664900";
+            readonly info: "#cb0659";
+            readonly brandPrimary: "#ffa5c0";
+            readonly brandPrimaryHover: "#f2497c";
+            readonly brandPrimarySelected: "#cb0659";
+            readonly locked: "#ffffff";
+            readonly lockedBrandPrimary: "#eb0055";
+            readonly lockedBrandSecondary: "#320096";
+        };
+        readonly icon: {
+            readonly default: "#ffffff";
+            readonly disabled: "#cbcdd2";
+            readonly subtle: "#cbcdd2";
+            readonly info: "#ffa5c0";
+            readonly error: "#f83552";
+            readonly success: "#2cbe6e";
+            readonly warning: "#ffef63";
+            readonly brandPrimary: "#ffa5c0";
+            readonly brandPrimaryHover: "#f2497c";
+            readonly inverted: "#161617";
+            readonly locked: "#ffffff";
+        };
+    };
+    readonly separator: {
+        readonly color: {
+            readonly default: "#90949d";
+            readonly subtle: "#696a6f";
+        };
+    };
+    readonly typography: {
+        readonly title1: {
+            readonly fontFamily: "Montserrat-Bold";
+            readonly fontSize: "1.75rem";
+            readonly lineHeight: "2.8rem";
+        };
+        readonly title2: {
+            readonly fontFamily: "Montserrat-Bold";
+            readonly fontSize: "1.3125rem";
+            readonly lineHeight: "2.1rem";
+        };
+        readonly title3: {
+            readonly fontFamily: "Montserrat-Bold";
+            readonly fontSize: "1.1875rem";
+            readonly lineHeight: "1.9rem";
+        };
+        readonly title4: {
+            readonly fontFamily: "Montserrat-Bold";
+            readonly fontSize: "1.0625rem";
+            readonly lineHeight: "1.7rem";
+        };
+        readonly body: {
+            readonly fontFamily: "Montserrat-Medium";
+            readonly fontSize: "1rem";
+            readonly lineHeight: "1.6rem";
+        };
+        readonly bodyS: {
+            readonly fontFamily: "Montserrat-Medium";
+            readonly fontSize: "0.875rem";
+            readonly lineHeight: "1.4rem";
+        };
+        readonly bodyXs: {
+            readonly fontFamily: "Montserrat-Medium";
+            readonly fontSize: "0.75rem";
+            readonly lineHeight: "1.2rem";
+        };
+        readonly bodyAccent: {
+            readonly fontFamily: "Montserrat-SemiBold";
+            readonly fontSize: "1rem";
+            readonly lineHeight: "1.6rem";
+        };
+        readonly bodyAccentS: {
+            readonly fontFamily: "Montserrat-SemiBold";
+            readonly fontSize: "0.875rem";
+            readonly lineHeight: "1.4rem";
+        };
+        readonly bodyAccentXs: {
+            readonly fontFamily: "Montserrat-SemiBold";
+            readonly fontSize: "0.75rem";
+            readonly lineHeight: "1.2rem";
+        };
+        readonly bodyItalic: {
+            readonly fontFamily: "Montserrat-MediumItalic";
+            readonly fontSize: "1rem";
+            readonly lineHeight: "1.6rem";
+        };
+        readonly bodyItalicAccent: {
+            readonly fontFamily: "Montserrat-SemiBoldItalic";
+            readonly fontSize: "1rem";
+            readonly lineHeight: "1.6rem";
+        };
+        readonly button: {
+            readonly fontFamily: "Montserrat-Bold";
+            readonly fontSize: "1rem";
+            readonly lineHeight: "1.6rem";
+        };
+    };
+};
+export type ColorsType = "#ffffff" | "#cbcdd2" | "#f83552" | "#f1f1f4" | "#2cbe6e" | "#ffa5c0" | "#f2497c" | "#c1a3ff" | "#6123df" | "#161617" | "#90949d" | "#696a6f" | "#700118" | "#09532d" | "#664900" | "#cb0659" | "#eb0055" | "#320096" | "#ffef63";
---
+++ dist/build/pro/index.light.web.js
@@ -0,0 +1,138 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ * Generated on Thu, 03 Apr 2025 13:31:09 GMT
+ */
+export const theme = {
+    "color": {
+        "text": {
+            "default": "#161617",
+            "subtle": "#696a6f",
+            "error": "#a20121",
+            "disabled": "#696a6f",
+            "success": "#15884f",
+            "brandPrimary": "#6123df",
+            "brandPrimaryHover": "#320096",
+            "brandSecondary": "#320096",
+            "brandSecondaryHover": "#25026c",
+            "inverted": "#ffffff",
+            "locked": "#ffffff"
+        },
+        "border": {
+            "default": "#90949d",
+            "subtle": "#cbcdd2",
+            "disabled": "#f1f1f4",
+            "error": "#a20121",
+            "selected": "#161617",
+            "focused": "#161617",
+            "brandPrimary": "#6123df",
+            "brandPrimaryHover": "#320096"
+        },
+        "outline": {
+            "default": "#161617",
+            "error": "#a20121",
+            "inverted": "#ffffff"
+        },
+        "background": {
+            "default": "#ffffff",
+            "subtle": "#f1f1f4",
+            "disabled": "#f1f1f4",
+            "error": "#fdf4f6",
+            "success": "#eaf8f0",
+            "warning": "#fff8df",
+            "info": "#f3edff",
+            "brandPrimary": "#320096",
+            "brandPrimaryHover": "#25026c",
+            "brandPrimarySelected": "#f3edff",
+            "locked": "#161617",
+            "lockedBrandPrimary": "#320096",
+            "lockedBrandSecondary": "#320096"
+        },
+        "icon": {
+            "default": "#161617",
+            "disabled": "#696a6f",
+            "subtle": "#696a6f",
+            "info": "#320096",
+            "error": "#a20121",
+            "success": "#15884f",
+            "warning": "#e1a605",
+            "brandPrimary": "#6123df",
+            "brandPrimaryHover": "#320096",
+            "inverted": "#ffffff",
+            "locked": "#ffffff"
+        }
+    },
+    "separator": {
+        "color": {
+            "default": "#90949d",
+            "subtle": "#cbcdd2"
+        }
+    },
+    "typography": {
+        "title1": {
+            "fontFamily": "Montserrat-Bold",
+            "fontSize": "1.75rem",
+            "lineHeight": "2.8rem"
+        },
+        "title2": {
+            "fontFamily": "Montserrat-Bold",
+            "fontSize": "1.3125rem",
+            "lineHeight": "2.1rem"
+        },
+        "title3": {
+            "fontFamily": "Montserrat-Bold",
+            "fontSize": "1.1875rem",
+            "lineHeight": "1.9rem"
+        },
+        "title4": {
+            "fontFamily": "Montserrat-Bold",
+            "fontSize": "1.0625rem",
+            "lineHeight": "1.7rem"
+        },
+        "body": {
+            "fontFamily": "Montserrat-Medium",
+            "fontSize": "1rem",
+            "lineHeight": "1.6rem"
+        },
+        "bodyS": {
+            "fontFamily": "Montserrat-Medium",
+            "fontSize": "0.875rem",
+            "lineHeight": "1.4rem"
+        },
+        "bodyXs": {
+            "fontFamily": "Montserrat-Medium",
+            "fontSize": "0.75rem",
+            "lineHeight": "1.2rem"
+        },
+        "bodyAccent": {
+            "fontFamily": "Montserrat-SemiBold",
+            "fontSize": "1rem",
+            "lineHeight": "1.6rem"
+        },
+        "bodyAccentS": {
+            "fontFamily": "Montserrat-SemiBold",
+            "fontSize": "0.875rem",
+            "lineHeight": "1.4rem"
+        },
+        "bodyAccentXs": {
+            "fontFamily": "Montserrat-SemiBold",
+            "fontSize": "0.75rem",
+            "lineHeight": "1.2rem"
+        },
+        "bodyItalic": {
+            "fontFamily": "Montserrat-MediumItalic",
+            "fontSize": "1rem",
+            "lineHeight": "1.6rem"
+        },
+        "bodyItalicAccent": {
+            "fontFamily": "Montserrat-SemiBoldItalic",
+            "fontSize": "1rem",
+            "lineHeight": "1.6rem"
+        },
+        "button": {
+            "fontFamily": "Montserrat-Bold",
+            "fontSize": "1rem",
+            "lineHeight": "1.6rem"
+        }
+    }
+};
+//# sourceMappingURL=index.light.web.js.map
