286165255f870f4ad8dfd4e6df80e84dcd1fce69
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-05-14 12:54:47 +0200

    v0.0.30

---
+++ dist/build/jeune/index.dark.mobile.js
@@ -0,0 +1,182 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ * Generated on Wed, 14 May 2025 07:21:22 GMT
+ */
+export const theme = {
+    "color": {
+        "text": {
+            "default": "#ffffff",
+            "subtle": "#cbcdd2",
+            "error": "#f83552",
+            "disabled": "#f1f1f4",
+            "success": "#2cbe6e",
+            "brandPrimary": "#ffa5c0",
+            "brandPrimaryHover": "#f2497c",
+            "brandSecondary": "#c1a3ff",
+            "brandSecondaryHover": "#6123df",
+            "inverted": "#161617",
+            "lockedInverted": "#ffffff",
+            "locked": "#161617"
+        },
+        "border": {
+            "default": "#cbcdd2",
+            "subtle": "#90949d",
+            "disabled": "#696a6f",
+            "error": "#f83552",
+            "selected": "#ffffff",
+            "focused": "#ffffff",
+            "brandPrimary": "#ffa5c0",
+            "brandPrimaryHover": "#f2497c",
+            "brandSecondary": "#c1a3ff",
+            "brandSecondaryHover": "#6123df",
+            "inverted": "#161617",
+            "decorative01": "#ffeae2",
+            "decorative02": "#dcf3f8",
+            "decorative03": "#dbffea",
+            "decorative04": "#c1a3ff",
+            "decorative05": "#ffa5c0"
+        },
+        "outline": {
+            "default": "#ffffff",
+            "error": "#f83552",
+            "inverted": "#161617"
+        },
+        "background": {
+            "default": "#161617",
+            "subtle": "#232323",
+            "disabled": "#696a6f",
+            "error": "#700118",
+            "success": "#09532d",
+            "warning": "#664900",
+            "info": "#cb0659",
+            "brandPrimary": "#ffa5c0",
+            "brandPrimaryHover": "#f2497c",
+            "brandPrimarySelected": "#cb0659",
+            "brandSecondary": "#c1a3ff",
+            "brandSecondaryHover": "#6123df",
+            "lockedInverted": "#161617",
+            "lockedBrandPrimary": "#eb0055",
+            "lockedBrandSecondary": "#320096",
+            "inverted": "#ffffff",
+            "decorative01": "#d63d00",
+            "decorative02": "#0e78b7",
+            "decorative03": "#0e8474",
+            "decorative04": "#25026c",
+            "decorative05": "#cb0659"
+        },
+        "icon": {
+            "default": "#ffffff",
+            "disabled": "#cbcdd2",
+            "subtle": "#cbcdd2",
+            "info": "#ffa5c0",
+            "error": "#f83552",
+            "success": "#2cbe6e",
+            "warning": "#ffef63",
+            "brandPrimary": "#ffa5c0",
+            "brandPrimaryHover": "#f2497c",
+            "brandSecondary": "#c1a3ff",
+            "brandSecondaryHover": "#6123df",
+            "inverted": "#161617",
+            "lockedInverted": "#ffffff"
+        }
+    },
+    "separator": {
+        "color": {
+            "default": "#90949d",
+            "subtle": "#696a6f"
+        }
+    },
+    "tag": {
+        "like": {
+            "color": {
+                "icon": "#ffa5c0",
+                "background": "#f1f1f4"
+            }
+        },
+        "new": {
+            "color": {
+                "background": "#ffa5c0"
+            }
+        },
+        "headline": {
+            "color": {
+                "icon": "#6123df",
+                "background": "#f3edff"
+            }
+        },
+        "bookclub": {
+            "color": {
+                "icon": "#0e78b7",
+                "background": "#dcf3f8"
+            }
+        }
+    },
+    "typography": {
+        "title1": {
+            "fontFamily": "Montserrat-Bold",
+            "fontSize": "28px",
+            "lineHeight": "44.8px"
+        },
+        "title2": {
+            "fontFamily": "Montserrat-Bold",
+            "fontSize": "21px",
+            "lineHeight": "33.6px"
+        },
+        "title3": {
+            "fontFamily": "Montserrat-Bold",
+            "fontSize": "19px",
+            "lineHeight": "30.4px"
+        },
+        "title4": {
+            "fontFamily": "Montserrat-Bold",
+            "fontSize": "17px",
+            "lineHeight": "27.2px"
+        },
+        "body": {
+            "fontFamily": "Montserrat-Medium",
+            "fontSize": "16px",
+            "lineHeight": "25.6px"
+        },
+        "bodyS": {
+            "fontFamily": "Montserrat-Medium",
+            "fontSize": "14px",
+            "lineHeight": "22.4px"
+        },
+        "bodyXs": {
+            "fontFamily": "Montserrat-Medium",
+            "fontSize": "12px",
+            "lineHeight": "19.2px"
+        },
+        "bodyAccent": {
+            "fontFamily": "Montserrat-SemiBold",
+            "fontSize": "16px",
+            "lineHeight": "25.6px"
+        },
+        "bodyAccentS": {
+            "fontFamily": "Montserrat-SemiBold",
+            "fontSize": "14px",
+            "lineHeight": "22.4px"
+        },
+        "bodyAccentXs": {
+            "fontFamily": "Montserrat-SemiBold",
+            "fontSize": "12px",
+            "lineHeight": "19.2px"
+        },
+        "bodyItalic": {
+            "fontFamily": "Montserrat-MediumItalic",
+            "fontSize": "16px",
+            "lineHeight": "25.6px"
+        },
+        "bodyItalicAccent": {
+            "fontFamily": "Montserrat-SemiBoldItalic",
+            "fontSize": "16px",
+            "lineHeight": "25.6px"
+        },
+        "button": {
+            "fontFamily": "Montserrat-Bold",
+            "fontSize": "16px",
+            "lineHeight": "25.6px"
+        }
+    }
+};
+//# sourceMappingURL=index.dark.mobile.js.map
